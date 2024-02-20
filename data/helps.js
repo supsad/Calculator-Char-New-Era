98487418af30883aa688734f9241d007362ed03d
Dmitry Void <supsad@yahoo.com>
2024-02-21 02:08:59 +0700

    Re-uploading the repository

---
+++ data/helps.js
@@ -0,0 +1,8 @@
+{
+    "attributes.power": "авыа",
+    "attributes.dexterity": "ааа",
+    "attributes.thinking": "ффф",
+    "attributes.education": "цуц",
+    "attributes.confidence": "23",
+    "attributes.endurance": "555",
+}
